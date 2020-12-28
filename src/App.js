83d0cbc044c963c3954a25a9d49959d2d778391e
ParkKiyoung <ky91zzang@naver.com>
2020-12-29 00:21:16 +0900

    component lifeCycle

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 //클래스형 컴포넌트 작성하기
 class App extends React.Component{ //이게 기본 뼈대임
+  //생명주기 알아봄
+  constructor(props){
+    super(props);
+    console.log('construct')
+  }
+
   state = {
     count : 0,
   }
-  //state는 객채형태의 데이터임, 동적 데이터를 저장하기 위함
-  //state는 반드시 소문자로 선언하고, 위치는 클래스형 컴포넌트 안에서 해야함
-
-  //카운트 조절하기 위한 함수
   add = ()=>{
     this.setState(current =>({
       count : current.count +1,
@@ -20,13 +22,25 @@ class App extends React.Component{ //이게 기본 뼈대임
     }))
   }
 
-  //함수형 컴포넌트는 JSX를 직접 return 하지만
-  //클래스형은 render 함수를 통해서 JSX를 return 함, 기본적으로 실행되는 함수임
+  componentDidMount(){
+    //컴포넌트가 마운트되고 난뒤
+    console.log('console rendered')
+  }
+  componentDidUpdate(){
+    //컴포넌트가 업데이트 되었을때
+    console.log('component updated')
+  }
+  componentWillUnmount(){
+    //컴포넌트가 화면에서 떠날때
+    console.log('component Unmount')
+  }
+
   render(){
+    console.log('render')
     return (
       <div>
         <h1>this number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button> {/*함수 적용*/}
+        <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
     )
