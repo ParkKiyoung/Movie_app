7122eae8a9acded91b84c567f48666b01f111497
ParkKiyoung <ky91zzang@naver.com>
2020-12-29 18:44:53 +0900

    detail-redirect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {HashRouter,Route} from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Navigation from './component/Navigation';
+import Detail from './routes/Detail';
 
 function App(){
   return (
@@ -13,6 +14,7 @@ function App(){
       {/* Link Router 컴포넌트는 반드시 HashRoute안에 있어야한다. */}
       <Route path="/about" component={About}/>
       <Route path="/" exact={true} component={Home}/>
+      <Route path="/movie-detail" exact={true} component={Detail}/>
       {/* Route에는 2가지 props를 전달할수 있는데 URL PATH, URL에 맞는 컴포넌트를 불러주기 위한 component props 
       path,component props를 통해 사용자가 접속한 URL을 보고 그에 맞는 컴포넌트를 화면에 그림
       Url/about으로 오게 되면 해당 컴포넌트로서 About을 호출한다고 봄
--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -6,7 +6,8 @@ function Navigation(){
     return(
         <div className="nav">
             <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            <Link to='/about'>About</Link>
+            {/* route props에 값을 담아 보내는 방법 */}
         </div>
     );
 }
